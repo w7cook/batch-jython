b6e22fbe698249b1127a7a190c8b4bb766cedc87
August <august@ubuntu.(none)>
2012-09-26 17:59:05 -0500

    A couple more test cases, testing remote for and remtoe if/else

--- test-files/test.py
+++ test-files/test.py
@@ -12,4 +12,12 @@ mybatch r in f :
         x = 0
         print "While"
     r.foo()
+    r.y
+    for i in [1, 2, 3]  :
+        r.call()
+        break
+    if x :
+        r.hello()
+    else :
+        r.bye()
 print "Hello World"
