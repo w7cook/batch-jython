e9ab36a7790314d1c6d336ff1a46251263933e5a
August Shi <cs.utexas.edu>
2012-09-29 17:19:55 -0500

    BatchRemote now attempts to connect to a server

--- test-files/BatchRemote.py
+++ test-files/BatchRemote.py
@@ -1,32 +1,54 @@
+import socket
+
 class BatchRemote :
     
+    def __init__(self) :
+       self.HOST = "localhost"
+       self.PORT = 9999
+       self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
     def execute(self, expr) :
+        try :
+            self.sock.connect((self.HOST, self.PORT))
+            self.sock.send(expr + "\n")
+            received = self.sock.recv(4096)
+            print "Received "  + str(received)
+        finally :
+            self.sock.close()
+        
         print expr
     
     def Var(self, name) :
-        return "(Var " + name + ")"
+        return name
     
     def Data(self, x) :
-        return "(Data " + str(x) + ")"
+        return x
     
     def Prim(self, op, args) :
-        return "(" + op + " " + str(args) + ")"
+        # Assume only binary op and SEQ for now
+        if (op == "SEQ") :
+            return " ; ".join(args)
+        else :
+            return "(" + str(args[0]) + " " + op + " " + str(args[1]) + ")"
     
     def Prop(self, base, field) :
-        return "(Prop " + base + " " +  field + ")"
+        return base + "." + field
     
     def Assign(self, op, target, source) :
-        return "(Assign " + op + " " + target + " " + source + ")"
+        return target + " " + op + "= " + source    # Consider case of just regular assign
     
     def Let(self, var, expression, body) :
-        return "(Let " + var + "=" + expression + " in " + body + ")"
+        return "var " + var + "=" + expression + "; " + body
     
     def If(self, condition, thenExp, elseExp) :
-        return "(If " + condition + " then " + thenExp + " else " + elseExp + ")"
+        return "if (" + str(condition) + ") {" + str(thenExp) + "} else {" + str(elseExp) + "}"
     
     def Loop(self, var, collection, body) :
-        return "(For " + var + " in " + collection + " do " + body + ")"
+        return "for (" + str(var) + " in " + str(collection) + ") {" + body + "}"
     
     def Call(self, target, method, args) :
-        return "(Call " + target + " " + method + " " + str(args) + ")"
+        return target + "." + method + "(" + ','.join(args) + ")"
+    
+    def Skip(self) :
+        return "skip"
 
